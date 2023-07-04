830dbaad01f22e7d772d12c21bf7c0de19d5e80f
WandaSimeoli <wandasimeoli@gmail.com>
2023-07-04 18:06:41 +0200

    Lista con array

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
+
+
 const { createApp } = Vue
 
   createApp({
     data() {
       return {
-        message: ''
+        title : 'TO DO LIST',
+        newtodo : '',
+         todolist : [
+          'Fare il carosello',
+          'Preparare il pranzo',
+          'Programmare vacanze',
+        ]
       }
-    }
+    },
+      methods: {
+        add() {
+          if(this.newtodo.trim !='') {
+            this.todolist.push(this.newtodo);
+            this.newtodo ='';
+          }
+        }
+      }
+    
   }).mount('#app')
